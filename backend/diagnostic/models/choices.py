b4947b06c056edb18e0393b2d324cbe4d87dc2c4
cylb0 <foucaut.morgan@gmail.com>
2026-06-08 21:30:19 +0200

    feat: moved ExposureChoice & SoilTypeChoice from PlantSubmission to choices.py. Both values are now nullable in PlantSubmission

--- backend/diagnostic/models/choices.py
+++ backend/diagnostic/models/choices.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+class ExposureChoice(models.TextChoices):
+    FULL_SUN = 'full_sun', 'Full sun'
+    PARTIAL_SHADE = 'partial_shade', 'Partial shade'
+    FULL_SHADE = 'full_shade', 'Full shade'
+
+class SoilTypeChoice(models.TextChoices):
+    CLAY = 'clay', 'clay'
+    SANDY = 'sandy', 'sandy'
+    LOAMY = 'loamy', 'loamy'
+    CHALKY = 'chalky', 'chalky'
+
 class PlantTypeChoice(models.TextChoices):
     APPLE = 'apple', _('Apple')
     POTATO = 'potato', _('Potato')
