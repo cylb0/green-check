008375162c50464efa371565dbd9b7a22820601f
cylb0 <foucaut.morgan@gmail.com>
2026-06-08 18:06:14 +0200

    feat: unit tests for PlantSubmission model

--- backend/diagnostic/models/__init__.py
+++ backend/diagnostic/models/__init__.py
@@ -1,9 +1,11 @@
 from .plan import Plan
+from .plant_submission import PlantSubmission
 from .subscription import Subscription
 from .user import User
 
 __all__ = [
     'Plan',
+    'PlantSubmission',
     'Subscription',
     'User',
     ]
