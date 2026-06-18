de4f11909e3277e29ab32f4d42269d0e90bd7e6f
cylb0 <foucaut.morgan@gmail.com>
2026-06-18 11:11:24 +0200

    feat: added LOCALE_PATH to settings.py, DiagnosticOut now returns translated fields

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -122,12 +122,13 @@ LANGUAGES = [
     ('fr', 'Français')
 ]
 
+LOCALE_PATHS = [BASE_DIR / 'locale']
+
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
