57903ac4817f7f620c5ea5d17d62b9b156ed9280
cylb0 <foucaut.morgan@gmail.com>
2026-06-25 09:49:50 +0200

    refactor: extracted code from apply_advice and moved it to AdviceRuleQuerySet. + Unit tests

--- backend/diagnostic/models/advice_rule.py
+++ backend/diagnostic/models/advice_rule.py
@@ -1,6 +1,30 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from diagnostic.models.choices import DiseaseLabelChoice, ExposureChoice, PlantTypeChoice, SoilTypeChoice
+from django.db.models import Case, When, IntegerField, Value
+
+class AdviceRuleQuerySet(models.QuerySet):
+    def best_match(self, disease_label, plant_type, soil_type, exposure):
+        return (
+            self.filter(disease_label=disease_label)
+            .filter(
+                models.Q(plant_type=plant_type) | models.Q(plant_type=None),
+                models.Q(soil_type=soil_type) | models.Q(soil_type=None),
+                models.Q(exposure=exposure) | models.Q(exposure=None),
+            )
+            .annotate(
+                specificity=Case(
+                    When(plant_type=plant_type, soil_type=soil_type, exposure=exposure, then=Value(4)),
+                    When(plant_type=plant_type, soil_type=soil_type, exposure=None, then=Value(3)),
+                    When(plant_type=plant_type, soil_type=None, exposure=None, then=Value(2)),
+                    When(plant_type=None, soil_type=None, exposure=None, then=Value(1)),
+                    default=Value(0),
+                    output_field=IntegerField()
+                )
+            )
+            .order_by('-specificity')
+            .first()
+        )
 
 class AdviceRule(models.Model):
     class SeverityChoice(models.TextChoices):
@@ -15,6 +39,8 @@ class AdviceRule(models.Model):
     soil_type = models.CharField(max_length=20, choices=SoilTypeChoice.choices, null=True, blank=True)
 
     advice_text = models.TextField(null=False, blank=False)
+    
+    objects = AdviceRuleQuerySet.as_manager()
 
     class Meta:
         constraints = [
